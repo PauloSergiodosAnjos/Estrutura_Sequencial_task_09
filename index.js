847d963157800cc2517e3a68f7a66b6f4189f463
Paulo Sergio <ngcpaulo1@gmail.com>
2023-04-04 22:11:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// 9
+let question = Number(prompt('Digite a medida do raio'));
+
+function areaCirculo() {
+    return 3.14 * (question ** 2);
+}
+console.log('A área do seu círculo é', areaCirculo());
+
+
+
+
